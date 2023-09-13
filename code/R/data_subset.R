ee03b8697b1714e4087518d3d00d1fdd30607ba9
Mark Novak <mnovak.m@gmail.com>
2023-09-13 09:31:17 -0700

    Additions to data_subset

--- code/R/data_subset.R
+++ code/R/data_subset.R
@@ -1,65 +1,115 @@
-# Pull out only datasets which we want to analyze
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.2){
+  if(is.numeric(x)){abs(x - round(x)) < tol}else{NA}
+  }
 
-subset_data <- function(datasets, exportSummaries = FALSE, dir = NULL){
+# Pull out only datasets which we want to analyze
+subset_data <- function(datasets, exportSummaries = FALSE, 
+                        dir = '../../temp/ErrorSummaries/'){
   
   if(is.null(dir)){dir <- ''}
   
   datasetIDs <- unlist(lapply(datasets, function(x){ x$study.info$datasetID} ))
   datasetNames <- unlist(lapply(datasets, function(x){ x$study.info$datasetName} )) 
   
-  skip.datasets <- c(664, 665, 980) # for some reason doesn't play with Holling.n
+  # insert datasetIDs to skip specific datasets
+  skip.datasets <- c()
   skip.datasets <- datasetIDs %in% skip.datasets
   
   replacement.study <- 
     unlist(lapply(datasets, function(x){ 
     x$study.info$replacement } ))
   
-  more.eaten.than.given <- 
-    replacement.study & 
+  non.numeric.predators <- 
     unlist(lapply(datasets, function(x){
-      any(x$data[, grep('Nconsumed', colnames(x$data))[1]] > x$data$Nprey) }))
+      any(is.na(x$data$Npredator) | x$data$Npredator == '') } ))
   
-  non.integer.prey <- 
+  # more.eaten.than.given <- 
+  #   !replacement.study & 
+  #   !non.numeric.predators &
+  #   unlist(lapply(datasets, function(x){
+  #     any(x$data[, grep('Nconsumed', colnames(x$data))[1]] > x$data$Nprey) }))
+  # 
+  # more.eaten.than.given.num <- 
+  #   unlist(lapply(datasets[more.eaten.than.given], function(x){
+  #     paste(
+  #       sum(x$data[, grep('Nconsumed', colnames(x$data))[1]] > x$data$Nprey),
+  #       'of', nrow(x$data))}))
+  
+  more.eaten.than.given <-
     unlist(lapply(datasets, function(x){
-      any(x$data$Nprey != round(as.numeric(x$data$Nprey))) } ))
+      x$study.info$rescaled.prey } ))
+  
+  more.eaten.than.given.num <-
+    unlist(lapply(datasets, function(x){
+      x$study.info$rescaled.prey.scaling.factor } ))
   
   insufficient.trtmts <- 
     unlist(lapply(datasets, function(x){
       length(unique(x$data$Nprey)) <  3 } ))
   
+  mass.study <-
+    unlist(lapply(datasets, function(x){
+      x$study.info$mass.study } ))
+  
+  non.integer.prey <- 
+    !mass.study &
+    unlist(lapply(datasets, function(x){
+      any(!is.wholenumber(x$data$Nprey)) } ))
+  
+  non.mean.study <-
+    unlist(lapply(datasets, function(x){
+      x$study.info$type != 'mean' } ))
+  
+  non.integer.eaten <- 
+    non.mean.study &
+    !mass.study &
+    !non.numeric.predators &
+    unlist(lapply(datasets, function(x){
+      any(!is.wholenumber(x$data$Nconsumed)) } ))
+  
   
   # ----->  EDIT HERE AS DESIRED  <------ #
   keep <- 
     replacement.study &
-    !more.eaten.than.given &
+    # !more.eaten.than.given &
     !non.integer.prey &
+    !non.integer.eaten &
+    !non.numeric.predators &
     !insufficient.trtmts &
+    !mass.study &
     !skip.datasets
   # ----------------------------------   #  
 
   if(exportSummaries){
-    write.table( cbind( datasetIDs[keep], 
-                        datasetNames[keep] ), 
-                paste0(dir, 'aaaaNotSkipped.txt'), 
+    write.table( datasetNames[keep], 
+                paste0(dir, 'aNotSkipped.txt'), 
                 row.names = FALSE)
-    write.table( cbind( datasetIDs[!keep], 
-                        datasetNames[!keep] ),  
-                paste0(dir, 'aaaaSkipped.txt'), 
+    write.table( datasetNames[!keep],  
+                paste0(dir, 'aSkipped.txt'), 
                 row.names = FALSE)
-    write.table( cbind( datasetIDs[more.eaten.than.given], 
-                        datasetNames[more.eaten.than.given] ), 
-                paste0(dir, 'aaaTooManyEaten.txt'),
+    write.table( cbind(datasetNames[more.eaten.than.given], 
+                       more.eaten.than.given.num[more.eaten.than.given.num > 0 & 
+                                                   !is.na(more.eaten.than.given.num)]), 
+                paste0(dir, 'TooManyEaten.txt'),
                 row.names = FALSE)
-    write.table( cbind( datasetIDs[non.integer.prey], 
-                        datasetNames[non.integer.prey] ),
-                paste0(dir, 'aaaNonIntegerPrey.txt'),
+    write.table( datasetNames[non.integer.prey],
+                paste0(dir, 'NonIntegerPrey.txt'),
                 row.names = FALSE)
-    write.table( cbind( datasetIDs[insufficient.trtmts], 
-                        datasetNames[insufficient.trtmts] ),
-                paste0(dir, 'aaaInsufficientTreatments.txt'),
+    write.table( datasetNames[non.integer.eaten],
+                 paste0(dir, 'NonIntegerEaten.txt'),
+                 row.names = FALSE)
+    write.table( datasetNames[non.numeric.predators],
+                 paste0(dir, 'NonNumericPredators.txt'),
+                 row.names = FALSE)
+    write.table( datasetNames[insufficient.trtmts],
+                paste0(dir, 'InsufficientTreatments.txt'),
                 row.names = FALSE)
+    write.table( datasetNames[mass.study],
+                 paste0(dir, 'MassStudy.txt'),
+                 row.names = FALSE)
   }
   
+  message(paste0('Total number of potential datasets: ', length(datasets)))
   message(paste0('Count of skipped datasets: ', sum(!keep)))
   message(paste0('Count of not-skipped datasets: ', sum(keep)))
 
